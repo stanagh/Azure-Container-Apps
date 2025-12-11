cacd6fc6f417074839d31848f5efa0261e4c80e7
Stan <oderaghara5@icloud>
2025-12-11 21:26:15 +0000

    Add locals for Key Vault secrets and remove unused variables

---
+++ terraform/modules/keyvault/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  kv_secrets = {
+    mongo_connstr = "mongo-connstr"
+    mongo_db_name           = "mongo-db-name"
+    redis_host              = "redis-host"
+    weather_api_key         = "weather-api-key"
+  }
+}
